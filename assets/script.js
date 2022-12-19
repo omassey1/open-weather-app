70026d675ac3c06a1698232689a24fbe89ed09c6
omassey1 <65238159+omassey1@users.noreply.github.com>
2022-12-19 10:54:22 -0500

    api calls

--- assets/script.js
+++ assets/script.js
@@ -5,3 +5,19 @@ let todayDate = document.getElementById("todayDate");
 let cityForm = document.getElementById("formCity"); // form for input
 let buttons = document.getElementById("buttons"); // buttons past search
 let cityEl = document.querySelector("#searchedCity"); // city as displayed
+
+// API CALLS
+
+// GET WEATHER FOR TODAY DISPLAY
+// My API Key: 4204bfdd6f4f063ef67429ec56df1142
+let getWeather = (city) => {
+  let apiURL1 = "://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=4204bfdd6f4f063ef67429ec56df1142";
+  fetch(apiURL1)
+    .then((response) => {
+        response.json()
+          .then((data) => {
+              // getWeather => showWeather
+              showWeather(data, city);
+            });
+      });
+};
